88689ebd1219f7cbb3f124e7b0c0b398136eef0a
onlyonezhongjinhui <88058373@qq.com>
2021-06-23 12:10:43 +0800

    从零搭建spring security oauth2认证授权体系（六）

--- 从零搭建spring security oauth2认证授权体系（五）/resource-server/src/main/java/io/github/oauth2/controller/GoodsController.java
+++ 从零搭建spring security oauth2认证授权体系（五）/resource-server/src/main/java/io/github/oauth2/controller/GoodsController.java
@@ -1,7 +1,6 @@
 package io.github.oauth2.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -12,25 +11,21 @@ import org.springframework.web.bind.annotation.*;
 public class GoodsController {
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public String post() {
         return "post goods";
     }
 
     @PutMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public String put() {
         return "put goods";
     }
 
     @DeleteMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public String delete() {
         return "delete goods";
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('USER')")
     public String get() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return "get goods";
